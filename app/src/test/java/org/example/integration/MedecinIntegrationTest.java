c65fb7cd724bd5ddc2378b9d43819eed932472ae
nembotnd1u <nembotnd1u@etu.univ-lorraine.fr>
2025-02-03 09:00:58 +0100

    j'utilise h2 au lieu d epostgreSQL pour lancer les tests et j'iai uene erreur différente de celle que j'avais précedemment. Pourquoi ?

--- app/src/test/java/org/example/integration/MedecinIntegrationTest.java
+++ app/src/test/java/org/example/integration/MedecinIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,6 +22,8 @@ import java.util.List;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
+//@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@TestPropertySource(locations = "classpath:application-test.yaml")
 class MedecinIntegrationTest {
     
     @Autowired
